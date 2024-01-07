a1d11a44ed8ecf167d6446069953a5118995f173
Niv Ezra <niv.cyber@gmail.com>
2024-01-07 22:40:16 +0200

    Finished functionallity of sending title info

--- bot.js
+++ bot.js
@@ -37,7 +37,6 @@ bot.onTextMessage('', async messageBody => {
 				},
 			];
 		});
-		console.log(JSON.stringify(inlineKeyboard, null, 2));
 		await bot.sendInlineKeyboard(
 			messageBody.chat.id,
 			'Choose a movie or a TV show',
@@ -51,6 +50,57 @@ const getNResults = (results, start, n) => {
 };
 
 // handling search results
-bot.onButton('', async messageBody => {
-	console.log(messageBody);
+bot.onButton('', async callbackQuery => {
+	if (callbackQuery.data.startsWith('tt')) {
+		const titleId = callbackQuery.data;
+		const title = await getImdbTitle(titleId);
+		const titleName = title.aboveTheFoldData.titleText.text;
+		const titleType = title.aboveTheFoldData.titleType.text;
+		const yearRange = title.aboveTheFoldData.releaseYear;
+		let yearText = yearRange.year;
+		if (titleType === 'TV Series') {
+			yearText += `-${yearRange.endYear || ''}`;
+		}
+		const duration = title.aboveTheFoldData.runtime.displayableProperty.value.plainText;
+		const genres = title.aboveTheFoldData.genres.genres.map(g => g.text);
+
+		const plot = `\n${title.aboveTheFoldData.plot.plotText.plainText}`;
+		let stars = title.aboveTheFoldData.principalCredits.filter(
+			c => c.category.text === 'Stars',
+		);
+		stars = stars[0]?.credits.map(c => c.name.nameText.text);
+		stars = joinIfExist(stars, ', ') + '.\n';
+
+		const rating = '⭐️ ' + title.aboveTheFoldData.ratingsSummary.aggregateRating;
+		const voteCount = title.aboveTheFoldData.ratingsSummary.voteCount.toLocaleString();
+		const ranking = title.aboveTheFoldData.meterRanking;
+		const currentRank = ranking.currentRank;
+		const rankDif = ranking.rankChange.difference;
+		let rankingText = ranking.currentRank;
+		switch (ranking.rankChange.changeDirection) {
+			case 'UP':
+				rankingText = `🟢 ${currentRank} ↗️ ${rankDif}`;
+				break;
+			case 'DOWN':
+				rankingText = `🔴 ${currentRank} ↘️ ${rankDif}`;
+				break;
+			case 'FLAT':
+				rankingText = `⚪️ ${currentRank}`;
+				break;
+		}
+		const imdbUrl = `https://www.imdb.com/title/${titleId}`;
+		const messageLines = [
+			titleName,
+			joinIfExist([titleType, yearText, duration], ' • '),
+			genres.join(', '),
+			plot,
+			stars,
+			joinIfExist([rating, voteCount, rankingText], ' • '),
+			imdbUrl,
+		];
+
+		await bot.sendTextMessage(joinIfExist(messageLines, '\n'), callbackQuery.message.chat.id);
+	}
 });
+
+const joinIfExist = (arr, separator) => arr?.filter(Boolean).join(separator);
